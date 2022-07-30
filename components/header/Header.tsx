7b8a0048eb8782dcebdc4b18fcbe84eb66cf12c7
Sabbir Ahmed Siddiquee <sabbir@sapien.network>
2022-07-31 00:19:46 +0600

    feat: theme switcher added

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
 import { useRouter } from 'next/router';
 
 // icons
 import { CreditCardIcon, HomeIcon, SunIcon } from '@heroicons/react/solid';
+
+// context
+import { AppContext } from 'context/AppContext';
+
+// types
 
 const TABS = [
   { name: 'Home', href: '/', icon: HomeIcon },
@@ -14,9 +20,10 @@ const classNames = (...classes: any[]) => {
 
 const Header = ()=> {
 	const { asPath } = useRouter();
+	const { setTheme } = useContext<any>(AppContext);
 
   return (
-		<div className='shadow-md shadow-gray-900 relative'>
+		<div className='shadow-sm dark:shadow-md dark:shadow-gray-900 shadow-gray-300 relative'>
 			<nav className="-mb-px flex space-x-8 justify-center" aria-label="Tabs">
 				{TABS.map((tab) => (
 					<a
@@ -24,7 +31,7 @@ const Header = ()=> {
 						href={tab.href}
 						className={classNames(
 							asPath == tab.href
-								? 'border-green-500 text-white'
+								? 'border-green-500 dark:text-white'
 								: 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
 							'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm'
 						)}
@@ -32,7 +39,7 @@ const Header = ()=> {
 					>
 						<tab.icon
 							className={classNames(
-								asPath == tab.href ? 'text-white' : 'text-gray-400 group-hover:text-gray-500',
+								asPath == tab.href ? 'dark:text-white' : 'text-gray-400 group-hover:text-gray-500',
 								'-ml-0.5 mr-2 h-5 w-5'
 							)}
 							aria-hidden="true"
@@ -41,8 +48,11 @@ const Header = ()=> {
 					</a>
 				))}
 			</nav>
-			<button className='absolute right-5 top-4'>
-				<SunIcon className='text-white text-sm w-6' />
+			<button
+				className='absolute right-5 top-4'
+				onClick={setTheme}
+			>
+				<SunIcon className='dark:text-white text-sm w-6' />
 			</button>
 		</div>  
 	)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,16 @@ import type { AppProps } from 'next/app';
 // components
 import { AppLayout } from 'components';
 
+// context
+import ContextProvider from 'context/AppContext';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <ContextProvider>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ContextProvider>
   )
 }
 
